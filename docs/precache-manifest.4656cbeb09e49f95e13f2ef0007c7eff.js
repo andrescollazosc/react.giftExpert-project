e90f7566da95454f8b370e97415550584e2f98a6
Andres Collazos <andrescollazosc@gmail.com>
2020-09-25 17:30:18 -0500

    Added folder docs

---
+++ docs/precache-manifest.4656cbeb09e49f95e13f2ef0007c7eff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54eb8d6961e3fcb16cb20b954077ab1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deeed4a8405aca911379",
+    "url": "/static/css/main.31ad770b.chunk.css"
+  },
+  {
+    "revision": "dcf09d6ede3b15119fcb",
+    "url": "/static/js/2.a16041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a16041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deeed4a8405aca911379",
+    "url": "/static/js/main.8306b918.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
